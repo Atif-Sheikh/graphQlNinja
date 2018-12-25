0763f7a230701e0bec147d19acee75e618950e85
Atif-Sheikh <atifsiddiquissg@gmail.com>
2018-12-25 19:38:19 +0500

    Integarted apollo with react

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+// allow cross-origin requests
+app.use(cors());
 
 // connect with mlab database.
 mongoose.connect('mongodb://Atif:app123@ds141654.mlab.com:41654/graphqltodo', () => {
